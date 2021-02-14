63d49d7512b11dbabd9fe3343ad0b25a188afa21
zbx <3328940245@qq.com>
2021-02-14 15:28:07 +0800

    测试

---
+++ gt-parent2/src/main/java/com/gtparent/gtparent2/GtParent2Application.java
@@ -0,0 +1,13 @@
+package com.gtparent.gtparent2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtParent2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GtParent2Application.class, args);
+    }
+
+}
---
+++ gt-parent2/src/test/java/com/gtparent/gtparent2/GtParent2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gtparent.gtparent2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GtParent2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
